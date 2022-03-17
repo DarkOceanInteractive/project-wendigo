1cb2c4073615c33d74e588c94adf95ff8335e25f
Adrien Lucbert <42178413+adrienlucbert@users.noreply.github.com>
2022-03-17 23:48:04 +0100

    feat: basic camera movements with mouse

---
+++ Assets/Scripts/Gameplay/Player/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectWendigo
+{
+    public class CameraController : MonoBehaviour
+    {
+        public float mouseSensitivity = 1000f;
+        private float xRotation = 0f;
+        [SerializeField] private Transform playerBodyTransform;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * this.mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * this.mouseSensitivity * Time.deltaTime;
+
+            this.xRotation = Mathf.Clamp(this.xRotation - mouseY, -90f, 90f);
+
+            this.playerBodyTransform.Rotate(Vector3.up * mouseX);
+            this.transform.localRotation = Quaternion.Euler(this.xRotation, 0f, 0f);
+        }
+    }
+}
