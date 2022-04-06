bc2c17a2f9931ff0072a35f33f70e0c303213b0f
Andy Smit <andy.smit@kpnmail.nl>
2022-04-07 00:24:30 +0200

    feat: added item moving and deleting

--- Assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
+++ Assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
 public class EquipmentObject : ItemObject
 {
-    public float amounts; //used for Ammunition or Battery amounts
     public void Awake() {
         type = ItemType.Equipment;
     }
