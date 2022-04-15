80c29ee80665ed57a0cdb144baa5324392ac0b7f
Adrien Lucbert <42178413+adrienlucbert@users.noreply.github.com>
2022-04-15 19:11:35 +0200

    feat(player): shoulder and hand game objects
    An item held in hand now has a fixed position and rotates with the
    camera

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -66,26 +66,5 @@ namespace ProjectWendigo
         {
             this._motion += motion;
         }
-
-        /// <summary>
-        /// Transform a direction vector along the camera view direction.
-        /// </summary>
-        /// <param name="direction">Direction vector to transform</param>
-        /// <returns>Direction vector transformed along the camera view direction</returns>
-        private Vector3 TransformToCameraView(Vector3 direction)
-        {
-            // Extract the camera forward direction along the x and z axes.
-            Vector3 cameraForward = this._cameraTransform.forward;
-            Vector3 horizontalCameraForward = new Vector3(cameraForward.x, 0f, cameraForward.z).normalized;
-
-            // Extract the camera right direction along the x and z axes.
-            Vector3 cameraRight = this._cameraTransform.right;
-            Vector3 horizontalCameraRight = new Vector3(cameraRight.x, 0f, cameraRight.z).normalized;
-
-            return horizontalCameraForward * direction.z
-              + Vector3.up * direction.y
-              + horizontalCameraRight * direction.x;
-        }
-
     }
 }
