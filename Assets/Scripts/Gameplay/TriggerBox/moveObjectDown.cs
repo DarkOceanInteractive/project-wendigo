875e2d99cd0d3bcc69f7e613521a94c2aba2e0a3
Adrien Lucbert <42178413+adrienlucbert@users.noreply.github.com>
2022-04-22 06:56:34 +0200

    feat: rocks fall animation and screen shake

--- Assets/Scripts/Gameplay/TriggerBox/moveObjectDown.cs
+++ Assets/Scripts/Gameplay/TriggerBox/moveObjectDown.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class moveObjectDown : MonoBehaviour
+namespace ProjectWendigo
 {
-    [SerializeField]
-    public float amount;
-    private int counter = 0;
-    
-    //Move rocks down by 10
-    void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" && counter == 0)
-        Debug.Log("Rock fell");
-        counter++;
-        transform.position -= new Vector3(0,amount,0);
+    public class moveObjectDown : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private float _animationDuration;
+        [SerializeField] private Vector2Spline _animationCurve = new Vector2Spline();
+        private Vector3 _startPosition;
+        private float _animationStartTime;
+        private bool _hasBeenTriggered = false;
+
+        protected void Start()
+        {
+            this._startPosition = this.transform.position;
+        }
+
+        protected void OnDrawGizmosSelected()
+        {
+            this._animationCurve.DrawGizmos(this.transform.position);
+        }
+
+        public void OnTriggerBoxEnter(Collider other)
+        {
+            if (this._hasBeenTriggered || other.tag != "Player")
+                return;
+            this._hasBeenTriggered = true;
+            this._animationStartTime = Time.time;
+            this.StartCoroutine("Fall");
+        }
+
+        private IEnumerator Fall()
+        {
+            float elapsedTime = Time.time - this._animationStartTime;
+            while (elapsedTime <= this._animationDuration)
+            {
+                elapsedTime = Time.time - this._animationStartTime;
+                float t = elapsedTime / this._animationDuration;
+                Vector3 currentOffset = this._animationCurve.Interpolate(Vector3.zero, this._offset, t);
+                this.transform.position = this._startPosition + currentOffset;
+                yield return null;
+            }
+            this.transform.position = this._startPosition + this._offset;
+        }
     }
 }
