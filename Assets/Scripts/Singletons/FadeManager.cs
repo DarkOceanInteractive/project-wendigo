09ed64601539a0af86660b9594d4da2600234ec2
Jasper Vonk <s1122268@student.hsleiden.nl>
2022-04-22 07:46:30 +0200

    feat: add FadeManager

---
+++ Assets/Scripts/Singletons/FadeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+namespace ProjectWendigo
+{
+    
+    public class FadeManager : MonoBehaviour
+    {
+
+        public Animator animator;
+
+        public void FadeOutEffect(){
+            animator.SetTrigger("FadeOut");
+        }
+
+        public void FadeInEffect(){
+            animator.SetTrigger("FadeIn");
+        }
+    }
+}
