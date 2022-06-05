9d8eccaeaecc1b5d1c9c450954f81c8829c449e2
Andy Smit <andy.smit@kpnmail.nl>
2022-06-05 15:46:20 +0200

    feat: focus camera on object

---
+++ Assets/Cameras/CameraSwitcher.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour {
+    [SerializeField]
+    private InputAction action;
+    [SerializeField]
+    private CinemachineVirtualCamera player;
+    [SerializeField]
+    private CinemachineVirtualCamera target;
+
+    private Animator animator;
+    private bool playerCamera = true;
+
+    public void OnEnable() {
+        action.Enable();
+    }
+
+    public void OnDisable() {
+        action.Disable();
+    }
+
+    public void Start() {
+        action.performed += _ => SwitchCamera();
+    }
+
+    public void SwitchCamera() {
+        if (playerCamera) {
+            player.Priority = 0;
+            target.Priority = 1;
+        } else {
+            player.Priority = 1;
+            target.Priority = 0;
+        }
+        playerCamera = !playerCamera;
+    }
+}
