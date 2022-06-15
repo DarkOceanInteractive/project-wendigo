5a8ebf4dd5c3a74273bcd4cad61910deacf6f0d7
Adrien Lucbert <42178413+adrienlucbert@users.noreply.github.com>
2022-06-15 20:26:36 +0200

    feat: add single-use triggerbox event
    fix(church-event): nearing sound didn't spawn in the player's back

--- Assets/Scripts/Gameplay/TriggerBox/TriggerBox.cs
+++ Assets/Scripts/Gameplay/TriggerBox/TriggerBox.cs
@@ -6,12 +6,19 @@ namespace ProjectWendigo
     public class TriggerBox : MonoBehaviour
     {
         public UnityEvent<Collider> OnTriggerBoxEnter;
+        public UnityEvent<Collider> OnTriggerBoxEnterOnce;
         public UnityEvent<Collider> OnTriggerBoxStay;
         public UnityEvent<Collider> OnTriggerBoxExit;
+        public UnityEvent<Collider> OnTriggerBoxExitOnce;
 
         private void OnTriggerEnter(Collider other)
         {
             this.OnTriggerBoxEnter?.Invoke(other);
+            if (this.OnTriggerBoxEnterOnce.GetPersistentEventCount() > 0)
+            {
+                this.OnTriggerBoxEnter.Invoke(other);
+                Destroy(this.gameObject);
+            }
         }
 
         private void OnTriggerStay(Collider other)
@@ -22,6 +29,11 @@ namespace ProjectWendigo
         private void OnTriggerExit(Collider other)
         {
             this.OnTriggerBoxExit?.Invoke(other);
+            if (this.OnTriggerBoxExitOnce.GetPersistentEventCount() > 0)
+            {
+                this.OnTriggerBoxExitOnce.Invoke(other);
+                Destroy(this.gameObject);
+            }
         }
     }
 }
