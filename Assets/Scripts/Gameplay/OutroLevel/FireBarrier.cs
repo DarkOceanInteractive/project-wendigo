0ff3d11aeeff3ed628a03169c75862b41b861bbf
teresa113697 <teresa2002@hotmail.nl>
2022-06-19 17:09:04 +0200

    feat(barrier): add script to turn fire into barrier

---
+++ Assets/Scripts/Gameplay/OutroLevel/FireBarrier.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBarrier : MonoBehaviour
+{
+    private ParticleSystem particles;
+    private GameObject player;
+
+    public float mass = 3.0f;
+    private Vector3 impact = Vector3.zero;
+    private CharacterController _characterController;
+    public float force = 50f;
+
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        var collision = particles.collision;
+        collision.enabled = true;
+        collision.type = ParticleSystemCollisionType.World;
+        collision.mode = ParticleSystemCollisionMode.Collision3D;
+        collision.sendCollisionMessages = true;
+
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        _characterController = player.GetComponent<CharacterController>();
+        Debug.Log(particles);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if(other.tag == "Player")
+            AddImpact(new Vector3(-1f, 0, 0), force);
+
+    }
+
+    private void AddImpact(Vector3 direction, float force)
+    {
+        direction.Normalize();
+        impact += direction.normalized * force / mass;
+    }
+
+    void Update()
+    {
+        if(impact.magnitude > 0.2)
+            _characterController.Move(impact * Time.deltaTime);
+            impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
+    }
+}
