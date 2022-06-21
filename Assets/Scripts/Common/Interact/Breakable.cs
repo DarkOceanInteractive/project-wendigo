b3db64e9e8ec87dbbae5eb18cb78048d2ac150e9
Nehir Akbulut <nickohre@outlook.com>
2022-06-21 15:34:15 +0200

    feat: add animation compatibility
    includes Hack animation

--- Assets/Scripts/Common/Interact/Breakable.cs
+++ Assets/Scripts/Common/Interact/Breakable.cs
@@ -8,14 +8,14 @@ namespace ProjectWendigo
         [SerializeField] private GameObject _tool;
 
         [SerializeField] private Animator _toolAnimator;
-        [SerializeField] private string _toolAnimation;
+        [SerializeField] private string _toolAnimationTrigger;
 
         [SerializeField] private string _onInteractSFX;
 
         public override void OnLookAt(GameObject target)
         {
-            if (this._tool.activeSelf)
-                Singletons.Main.Interface.OpenMessagePanel("- Press F to pick up -");
+            if (this._tool.activeSelf || this._tool == null)
+                Singletons.Main.Interface.OpenMessagePanel("- Press F to break -");
         }
 
         public override void OnLookAway(GameObject target)
@@ -25,10 +25,10 @@ namespace ProjectWendigo
 
         public override void OnInteract(GameObject target)
         {
-            if (this._tool.activeSelf)
+            if (this._tool.activeSelf || this._tool == null)
             {
-                if (this._toolAnimator != null && this._toolAnimation != null)
-                    this._toolAnimator.SetTrigger(this._toolAnimation);
+                if (this._toolAnimator != null && this._toolAnimationTrigger != null)
+                    this._toolAnimator.SetTrigger(this._toolAnimationTrigger);
                 if (this._obstacle.activeSelf)
                     this._obstacle.SetActive(false);
                 if (this._onInteractSFX != "")
