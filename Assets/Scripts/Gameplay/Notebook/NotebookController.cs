4d07c3594162c49a3fc5df8cd8ad04e62b4637a5
Adrien Lucbert <42178413+adrienlucbert@users.noreply.github.com>
2022-06-21 16:39:09 +0200

    fix(notebook): cut audios and play swap on section swap
    fix(notebook): some sounds wouldn't play at the right time

--- Assets/Scripts/Gameplay/Notebook/NotebookController.cs
+++ Assets/Scripts/Gameplay/Notebook/NotebookController.cs
@@ -8,12 +8,16 @@ namespace ProjectWendigo
         {
             this.gameObject.SetActive(true);
             Singletons.Main.Input.ShowCursor();
+            Singletons.Main.Sound.Play("notebook_open");
+            Singletons.Main.Camera.LockCamera();
         }
 
         public void Close()
         {
             this.gameObject.SetActive(false);
             Singletons.Main.Input.HideCursor();
+            Singletons.Main.Sound.Play("notebook_close");
+            Singletons.Main.Camera.UnlockCamera();
         }
     }
 }
