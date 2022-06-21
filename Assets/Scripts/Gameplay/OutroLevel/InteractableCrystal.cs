12a268731c051abe04bca4a55532a85bb452629d
teresa113697 <teresa2002@hotmail.nl>
2022-06-21 20:39:46 +0200

    feat: add scripts to objects in scene

--- Assets/Scripts/Gameplay/OutroLevel/InteractableCrystal.cs
+++ Assets/Scripts/Gameplay/OutroLevel/InteractableCrystal.cs
@@ -7,22 +7,31 @@ namespace ProjectWendigo
     public class InteractableCrystal : AInteractable
     {
         [SerializeField] private GameObject crystal;
+        [SerializeField] private ParticleSystem powerBeam;
         [SerializeField] private string pickaxeSound;
 
         public override void OnLookAt(GameObject target)
         {
-            Singletons.Main.Interface.OpenMessagePanel("- Press F to destroy crystal -");
+            var options = new MessagePanelOptions
+            {
+                Text = $"- Press {Singletons.Main.Input.GetBinding("Player/Interact")} to destroy crystal -",
+                Location = MessagePanelLocation.BottomCenter
+            };
+            Singletons.Main.Interface.OpenMessagePanel(options);
         }
 
         public override void OnLookAway(GameObject target)
         {
+
             Singletons.Main.Interface.CloseMessagePanel();
         }
 
         public override void OnInteract(GameObject target)
         {
             if (this.crystal != null)
-                this.crystal.SetActive(false);
+                this.crystal.SetActive(false); // Remove crystal object
+            if (this.powerBeam.isPlaying)
+                powerBeam.Stop(); // Stop the power beam particles that came from the crystal object
             if (this.pickaxeSound != null)
                 Singletons.Main.Sound.Play(this.pickaxeSound);
         }
