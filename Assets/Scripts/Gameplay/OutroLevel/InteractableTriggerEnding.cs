56b531882144a4aa043320c9bd07668acbcfe58d
Andy Smit <andy.smit@kpnmail.nl>
2022-06-24 01:15:25 +0200

    feat: add credits and auto-quit game

--- Assets/Scripts/Gameplay/OutroLevel/InteractableTriggerEnding.cs
+++ Assets/Scripts/Gameplay/OutroLevel/InteractableTriggerEnding.cs
@@ -6,21 +6,23 @@ namespace ProjectWendigo
     public class InteractableTriggerEnding : AInteractable
     {
         public enum Characters { Daughter, Samodiva };
-        public Characters character;
-        public ParticleSystem screenParticles;
-        public GameObject focusObject;
-        public float minBlendTime = 7f;
-        public float maxBlendTime = 10f;
-        public ParticleSystem portal;
+        public Characters Character;
+        public ParticleSystem ScreenParticles;
+        public GameObject FocusObject;
+        public float MinBlendTime = 7f;
+        public float MaxBlendTime = 10f;
+        public ParticleSystem Portal;
+        public GameObject Credits;
+        public float CreditsScrollSpeed = 100f;
 
         void Start()
         {
-            screenParticles.Stop();
+            this.ScreenParticles.Stop();
         }
 
         public override void OnLookAt(GameObject target)
         {
-            if (character == Characters.Daughter)
+            if (this.Character == Characters.Daughter)
             {
                 var options = new MessagePanelOptions
                 {
@@ -42,17 +44,17 @@ namespace ProjectWendigo
 
         public override void OnInteract(GameObject target)
         {
-            StartCoroutine(FadeBlackOutSquare());
+            StartCoroutine(this.FadeBlackOutSquare());
 
-            screenParticles.Play();
-            ChangePortal();
+            this.ScreenParticles.Play();
+            this.ChangePortal();
             Singletons.Main.Camera.LockCamera();
-            Singletons.Main.Camera.FocusOnTarget(focusObject, minBlendTime, maxBlendTime);
+            Singletons.Main.Camera.FocusOnTarget(this.FocusObject, this.MinBlendTime, this.MaxBlendTime);
         }
 
         private void ChangePortal()
         {
-            var size = portal.sizeOverLifetime;
+            var size = Portal.sizeOverLifetime;
             size.enabled = true;
             AnimationCurve curve = new AnimationCurve();
             curve.AddKey(0.0f, 0.0f);
@@ -60,10 +62,37 @@ namespace ProjectWendigo
             size.size = new ParticleSystem.MinMaxCurve(1.5f, curve);
         }
 
-        public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true)
+        public IEnumerator FadeBlackOutSquare()
         {
             yield return new WaitForSecondsRealtime(5);
             Singletons.Main.Fade.FadeOutEffect();
+            Invoke(nameof(this.StartCredits), 4f);
+        }
+
+        private void StartCredits()
+        {
+            this.Credits.SetActive(true);
+            Invoke(nameof(this.StartCreditsScrolling), 5f);
+        }
+
+        private IEnumerator ScrollCredits()
+        {
+            while (true)
+            {
+                this.Credits.transform.Translate(Vector3.up * Time.deltaTime * this.CreditsScrollSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        private void StartCreditsScrolling()
+        {
+            StartCoroutine(this.ScrollCredits());
+            Invoke(nameof(this.EndGame), 30f);
+        }
+
+        private void EndGame()
+        {
+            Application.Quit();
         }
     }
 }
